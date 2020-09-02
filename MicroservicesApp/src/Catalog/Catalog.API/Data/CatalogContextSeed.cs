0f0d32c4e1dfd04387182ef8ec1ead59e8f5a757
Himanshi Gulati <himanshi.gulati@nagarro.com>
2020-09-02 23:25:05 +0530

    Configured mongoDB database and added CRUD methods in repository classes

---
+++ MicroservicesApp/src/Catalog/Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,47 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+            if (!existProduct)
+            {
+                productCollection.InsertManyAsync(GetPreConfiguredProducts());
+            }
+        }
+
+        private static IEnumerable<Product> GetPreConfiguredProducts()
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    Name = "Iphone X",
+                    Summary = "Summary",
+                    Description = "Description",
+                    ImageFile = "ImageFile",
+                    Price= 25,
+                    Category ="Phone"
+
+                },
+                new Product()
+                {
+                    Name = "Samsung",
+                    Summary = "Summary",
+                    Description = "Description",
+                    ImageFile = "ImageFile",
+                    Price= 25,
+                    Category ="Phone"
+                }
+            };
+        }
+    }
+}
